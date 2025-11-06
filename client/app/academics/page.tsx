ae2767f6c570883e7e24efe635f221fd2ac20700
Geeta Solanki <geetasolanki9244@gmail.com>
2025-11-06 18:50:13 +0530

    New!!

--- client/app/academics/page.tsx
+++
@@ -1,9 +0,0 @@
-import CoursesSection from "@/components/academics/CoursesSection";
-
-export default function AcademicsPage() {
-  return (
-    <div>
-      <CoursesSection sectionTitle="Academics — Categories" courses={[]} />
-    </div>
-  );
-}
